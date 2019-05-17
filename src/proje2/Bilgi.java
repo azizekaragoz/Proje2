1027fca6414099eb832c4b62abe0dc3733469b94
ሚካኤል <Michaeldkebede@gmail.com>
2019-05-17 13:43:41 +0300

    Master branch

---
+++ src/proje2/Bilgi.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proje2;
+
+/**
+ *
+ * @author Michael
+ */
+import java.util.ArrayList;
+
+public class Bilgi {
+    String adSoyad;
+    Date birthDate;
+    ArrayList<String> club = new ArrayList<String>();
+    
+    
+}
